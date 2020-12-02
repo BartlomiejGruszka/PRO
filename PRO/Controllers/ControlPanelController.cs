fdd43d0ff3407d9a79eb7e266b1d786576255bcd
bartlomiejgruszka <bartlomiej.gruszka@gmail.com>
2020-12-02 18:51:24 +0100

    general mod and author panels. specific cruds need adjustments

---
+++ PRO/Controllers/ControlPanelController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PRO.Controllers
+{
+    
+    public class ControlPanelController : Controller
+    {
+        // GET: panel/Admin
+        [Authorize(Roles = "Admin")]
+        [Route("panel/admin")]
+        public ActionResult Admin()
+        {
+            return View();
+        }
+
+        // GET: panel/moderator
+        [Authorize(Roles = "Moderator")]
+        [Route("panel/moderator")]
+        public ActionResult Moderator()
+        {
+            return View();
+        }
+
+        // GET: panel/Author
+        [Authorize(Roles = "Author")]
+        [Route("panel/author")]
+        public ActionResult Author()
+        {
+            return View();
+        }
+    }
+}
